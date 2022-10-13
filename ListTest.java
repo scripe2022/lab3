e974e0d0b6254fb68853c0dff0b85be32e1d9d94
scripe2022 <im@jyh.sb>
2022-10-13 11:34:37 -0700

    oct13 1132

---
+++ ListTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ListTest {
+    // @Test
+    // public void testFilter() {
+    //     List<String> input1 = new ArrayList<>();
+    // }
+
+    @Test
+    public void testMerge() {
+
+    }
+
+}
